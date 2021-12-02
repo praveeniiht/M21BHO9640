8d9b142771d8db0a00fd0312fb1198dab70c2bb8
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-12-02 14:37:02 +0530

    Add files via upload
    Springboot cloudgate way application

---
+++ springcloudgateway/src/main/java/com/example/microgateway/springcloudgateway/SpringcloudgatewayApplication.java
@@ -0,0 +1,19 @@
+package com.example.microgateway.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+
+public class SpringcloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudgatewayApplication.class, args);
+	}
+	
+	
+
+}
