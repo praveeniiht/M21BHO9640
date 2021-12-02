a4f28a81be776077a85025db912b36f21e45de2c
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-12-02 12:09:51 +0530

    Add files via upload
    Load Balancing application-1

---
+++ service1/src/main/java/com/example/service1/service1/ServiceController1.java
@@ -0,0 +1,23 @@
+package com.example.service1.service1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ServiceController1 {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/getmicro2")
+	  public String getMicro2Instance()
+	  {
+	        String url = "http://service2/mservice2/port";
+	        String port = "Currently hitting instance running on port: " + 
+	                       restTemplate.getForObject(url, String.class);
+	        return port;
+	  }
+
+}
